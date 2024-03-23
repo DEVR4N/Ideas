ed9b44724ab3a4f9f3e8013ca4ec0ad27533014d
devr4n <secondply.2@gmail.com>
2024-03-23 10:46:20 +0200

    #1 search fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,9 +11,14 @@ class DashboardController extends BaseController
 {
     public function index()
     {
-        return view('dashboard', [
-                'ideas' => Idea::orderBy('created_at', 'desc')->paginate(3),
-            ]);
+        $ideas = Idea::orderBy('created_at', 'DESC');
+
+        if (request()->has('search')) {
+            $ideas = $ideas->where('content', 'like', '%' . request()->get('search', '') . '%');
+        }
+
+        return view('dashboard', ['ideas' => $ideas->paginate(3)]);
     }
+
 
 }
