8639147c1b0eb38685d85532842b911e4bafa534
devr4n <devr4n@github.com>
2024-03-26 12:47:06 +0300

    #3 route group fix

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,33 +15,20 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::post('/ideas', [IdeaController::class, 'store'])->name('ideas.create');
+Route::group(['prefix' => 'ideas/', 'as'=>'ideas.'], function () {
 
-Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
+    Route::post('', [IdeaController::class, 'store'])->name('create')->withoutMiddleware('[auth]');
+    Route::get('{idea}', [IdeaController::class, 'show'])->name('show')->withoutMiddleware('[auth]');
+    Route::group(['middleware' => 'auth'], function () {
+        Route::get('create', [IdeaController::class, 'create'])->name('create');
+        Route::get('{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
+        Route::put('{idea}', [IdeaController::class, 'update'])->name('update');
+        Route::delete('/{idea}', [IdeaController::class, 'destroy'])->name('destroy');
+        Route::post('/{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
 
-Route::get('/ideas/{idea}/edit', [IdeaController::class, 'edit'])->name('ideas.edit')->middleware('auth');
-
-Route::put('/ideas/{idea}', [IdeaController::class, 'update'])->name('ideas.update')->middleware('auth');
-
-Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy')->middleware('auth');
-
-Route::post('/ideas/{idea}/comments', [CommentController::class, 'store'])->name('ideas.comments.store')->middleware('auth');
-
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-
-Route::post('/register', [AuthController::class, 'store']);
-
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-
-Route::post('/login', [AuthController::class, 'authenticate']);
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-Route::get('/terms', function () { return view('terms'); });
-
-
+    });
+});
 
 
