9585de5ca8591e56fb9d3ae1a4a7617a77398b32
devr4n <devr4n@github.com>
2024-04-11 13:55:31 +0300

    #11 left side bar select color

--- resources/views/admin/shared/left-side-bar.blade.php
+++ resources/views/admin/shared/left-side-bar.blade.php
@@ -2,25 +2,25 @@
     <div class="card-body pt-3">
         <ul class="nav nav-link-secondary flex-column fw-bold gap-2">
             <li class="nav-item">
-                <a class="{{ (Route::is('admin.dashboard')) ? 'text-white bg-primary rounded' : ''}} nav-link"
+                <a class="{{ (Route::is('admin.dashboard')) ? 'text-white bg-dark rounded' : ''}} nav-link"
                    href="{{route('admin.dashboard')}}">
                     <i class="fa-solid fa-chart-line"></i>
                     <span>Dashboard</span></a>
             </li>
             <li class="nav-item">
-                <a class="{{ (Route::is('admin.users.index')) ? 'text-white bg-primary rounded' : ''}} nav-link"
+                <a class="{{ (Route::is('admin.users.index')) ? 'text-white bg-dark rounded' : ''}} nav-link"
                    href="{{route('admin.users.index')}}">
                     <i class="fa-solid fa-users" style="color: #0f91f5;"></i>
                     <span>Users</span></a>
             </li>
             <li class="nav-item">
-                <a class="{{ (Route::is('admin.ideas.index')) ? 'text-white bg-primary rounded' : ''}} nav-link"
+                <a class="{{ (Route::is('admin.ideas.index')) ? 'text-white bg-dark rounded' : ''}} nav-link"
                    href="{{route('admin.ideas.index')}}">
                     <i class="fa-solid fa-lightbulb" style="color: #FFD43B;"></i>
                     <span>Ideas</span></a>
             </li>
             <li class="nav-item">
-                <a class="{{ (Route::is('admin.comments.index')) ? 'text-white bg-primary rounded' : ''}} nav-link"
+                <a class="{{ (Route::is('admin.comments.index')) ? 'text-white bg-dark rounded' : ''}} nav-link"
                    href="{{route('admin.comments.index')}}">
                     <i class="fa-solid fa-comments" style="color: #06d039;"></i>
                     <span>Comments</span></a>
