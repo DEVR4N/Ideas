d749234e91dc1478c4ca4c6352079ce3476724e0
devr4n <devr4n@github.com>
2024-04-11 13:40:14 +0300

    #11 login - register design fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,47 +2,106 @@
 @section('title', 'Register')
 @section('content')
     <div class="row justify-content-center">
-        <div class="col-12 col-sm-8 col-md-6">
-            <form class="form mt-5" action="{{route('register')}}" method="post">
-                @csrf
-                <h3 class="text-center text-dark">Register</h3>
-                <div class="form-group">
-                    <label for="name" class="text-dark">Name:</label><br>
-                    <input type="text" name="name" id="name" class="form-control">
-                    @error('name')
-                    <span class="d-block fs-6 text-danger mt-2">{{ $message }}</span>
-                    @enderror
+        <form class="form mt-5" action="{{route('register')}}" method="post">
+            @csrf
+
+            <section class="p-3 p-md-4 p-xl-5">
+                <div class="container">
+                    <div class="card border-light-subtle shadow-sm">
+                        <div class="row g-0">
+                            <div class="col-12 col-md-6 text-bg-dark">
+                                <div class="d-flex align-items-center justify-content-center h-100">
+                                    <div class="col-10 col-xl-8 py-3">
+                                        <i class="fa-solid fa-brain fa-beat-fade fa-2xl"></i>
+                                        <hr>
+                                        <h3 class="h3 mb-4">
+                                            A free social networking where users broadcast short posts as idea.
+                                        </h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <div class="card-body p-3 p-md-4 p-xl-5">
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="mb-5">
+                                                <h2 class="h3">Registration</h2>
+                                                <h3 class="fs-6 fw-normal text-secondary m-0">
+                                                    Enter your details to register
+                                                </h3>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <form action="#!">
+                                        <div class="row gy-3 gy-md-4 overflow-hidden">
+                                            <div class="col-12">
+                                                <div class="form-group">
+                                                    <label for="name" class="text-dark"> Name </label>
+                                                    <span class="text-danger">*</span>
+                                                    <input type="text" name="name" id="name" class="form-control"
+                                                           placeholder="Name">
+                                                    @error('name')
+                                                    <span class="d-block fs-6 text-danger mt-2">{{ $message }}</span>
+                                                    @enderror
+                                                </div>
+                                            </div>
+
+                                            <div class="col-12">
+                                                <label for="email" class="text-dark"> Email </label>
+                                                <span class="text-danger">*</span>
+                                                <input type="email" name="email" id="email" class="form-control"
+                                                       placeholder="mail@example.com" required>
+                                                @error('email')
+                                                <span class="d-block fs-6 text-danger mt-2">{{ $message }}</span>
+                                                @enderror
+                                            </div>
+                                            <div class="col-12">
+                                                <label for="password" class="text-dark"> Password </label>
+                                                <span class="text-danger">*</span>
+                                                <input type="password" name="password" id="password"
+                                                       class="form-control">
+                                                @error('password')
+                                                <span class="d-block fs-6 text-danger mt-2">{{ $message }}</span>
+                                                @enderror
+                                            </div>
+                                            <div class="col-12">
+                                                <label for="confirm-password" class="text-dark"> Confirm
+                                                    Password </label>
+                                                <span class="text-danger">*</span>
+                                                <input type="password" name="password_confirmation"
+                                                       id="confirm-password"
+                                                       class="form-control" required>
+                                                @error('password_confirmation')
+                                                <span class="d-block fs-6 text-danger mt-2">{{ $message }}</span>
+                                                @enderror
+                                            </div>
+
+                                            <div class="col-12">
+                                                <div class="d-grid">
+                                                    <button class="btn bsb-btn-xl btn-dark" type="submit">
+                                                        Sign up
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <hr class="mt-5 mb-4 border-secondary-subtle">
+                                            <p class="m-0 text-secondary text-center">Already have an account ?
+                                                <a href="/login" class="link-primary text-decoration-none">
+                                                    <u> Login </u>
+                                                </a>
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="form-group mt-3">
-                    <label for="email" class="text-dark">Email:</label><br>
-                    <input type="email" name="email" id="email" class="form-control">
-                    @error('email')
-                    <span class="d-block fs-6 text-danger mt-2">{{ $message }}</span>
-                    @enderror
-                </div>
-                <div class="form-group mt-3">
-                    <label for="password" class="text-dark">Password:</label><br>
-                    <input type="password" name="password" id="password" class="form-control">
-                    @error('password')
-                    <span class="d-block fs-6 text-danger mt-2">{{ $message }}</span>
-                    @enderror
-                </div>
-                <div class="form-group mt-3">
-                    <label for="confirm-password" class="text-dark">Confirm Password:</label><br>
-                    <input type="password" name="password_confirmation" id="confirm-password" class="form-control">
-                    @error('password_confirmation')
-                    <span class="d-block fs-6 text-danger mt-2">{{ $message }}</span>
-                    @enderror
-                </div>
-                <div class="form-group">
-                    <label for="remember-me" class="text-dark"></label><br>
-                    <input type="submit" name="submit" class="btn btn-dark btn-md" value="submit">
-                </div>
-                <div class="text-right mt-2">
-                    <a href="/login" class="text-dark">Login here</a>
-                </div>
-            </form>
-        </div>
+            </section>
+        </form>
     </div>
 @endsection
-
